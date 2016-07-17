c42761f20c496d29fd110d56844c562b072e1a57
Matthew Tse <mtse9779@gmail.com>
2016-07-16 21:05:46 -0400

    validate not yet working

--- app/src/main/java/com/medibook/medibook/activity/RegisterMainActivity.java
+++ app/src/main/java/com/medibook/medibook/activity/RegisterMainActivity.java
@@ -4,12 +4,16 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.view.KeyEvent;
+import android.widget.TextView;
+import android.view.inputmethod.EditorInfo;
 
 import com.medibook.medibook.R;
 import com.medibook.medibook.common.API;
@@ -21,10 +25,10 @@ public class RegisterMainActivity extends AppCompatActivity implements View.OnCl
     private EditText etFname;
     private EditText etLname;
     private EditText etAddress;
-    private EditText etGender;
     private EditText etDOB;
     private EditText etEmail;
     private EditText etPassword;
+    private EditText etREPassword;
     private EditText etHCNum;
     private Button butNext;
     private RadioGroup rbg;
@@ -40,10 +44,20 @@ public class RegisterMainActivity extends AppCompatActivity implements View.OnCl
         etFname = (EditText) findViewById(R.id.etFname);
         etLname = (EditText) findViewById(R.id.etLname);
         etAddress = (EditText) findViewById(R.id.etAddress);
-//        etGender = (EditText) findViewById(R.id.etGender);
         etDOB = (EditText) findViewById(R.id.etDOB);
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        etREPassword = (EditText) findViewById(R.id.etREPassword);
+        etREPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.validatePassword || id == EditorInfo.IME_NULL) {
+                    validate();
+                    return true;
+                }
+                return false;
+            }
+        });
         etHCNum = (EditText) findViewById(R.id.etHCNumber);
         butNext = (Button) findViewById(R.id.butNext);
         rbg=(RadioGroup) findViewById(R.id.radioGroup1);
@@ -98,5 +112,29 @@ public class RegisterMainActivity extends AppCompatActivity implements View.OnCl
             dpd.show();
             System.out.println("Year: " + mYear + " Month: " + mMonth + " Day: " + mDay);
         }
+    }
+    private void validate(){
+        etREPassword.setError(null);
+        String password1 = etREPassword.getText().toString();
+        String password2 = etPassword.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password1) && !isPasswordSame(password1, password2)) {
+            etREPassword.setError(getString(R.string.error_password_mismatch));
+            focusView = etREPassword;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        }
+    }
+    private boolean isPasswordSame(String password1, String password2) {
+        return password1.equals(password2);
     }
 }

923c00c6a6304e7287d5301765c19c316aa3b886
Jesper Leung <jesperleung23@gmail.com>
2016-07-19 14:06:31 -0400

    1st register page working error checking

--- app/src/main/java/com/medibook/medibook/activity/RegisterMainActivity.java
+++ app/src/main/java/com/medibook/medibook/activity/RegisterMainActivity.java
@@ -25,6 +25,7 @@ public class RegisterMainActivity extends AppCompatActivity implements View.OnCl
     private EditText etDOB;
     private EditText etEmail;
     private EditText etPassword;
+    private EditText etPassword_2;
     private EditText etHCNum;
     private Button butNext;
     private RadioGroup rbg;
@@ -42,6 +43,7 @@ public class RegisterMainActivity extends AppCompatActivity implements View.OnCl
         etDOB = (EditText) findViewById(R.id.etDOB);
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        etPassword_2 = (EditText) findViewById(R.id.etPassword_2);
         etHCNum = (EditText) findViewById(R.id.etHCNumber);
 
         butNext = (Button) findViewById(R.id.butNext);
@@ -51,31 +53,69 @@ public class RegisterMainActivity extends AppCompatActivity implements View.OnCl
         etDOB.setOnClickListener(this);
     }
 
-    private void registerUser() {
+    private boolean registerUser() {
+        if (etFname.length() == 0 || etLname.length() == 0 || etAddress.length() == 0 || etDOB.length() == 0 || etEmail.length() == 0 || etPassword.length() == 0 || etPassword_2.length() == 0 || etHCNum.length() == 0) {
+            if (etFname.length() == 0) {
+                etFname.setError("Cannot be empty");
+            }
+            if (etLname.length() == 0) {
+                etLname.setError("Cannot be empty");
+            }
+            if (etAddress.length() == 0) {
+                etAddress.setError("Cannot be empty");
+            }
+            if (etDOB.length() == 0) {
+                etDOB.setError("Cannot be empty");
+            }
+            if (etEmail.length() == 0) {
+                etEmail.setError("Cannot be empty");
+            }
+            if (etPassword.length() == 0) {
+                etPassword.setError("Cannot be empty");
+            }
+            if (etPassword_2.length() == 0) {
+                etPassword_2.setError("Cannot be empty");
+            }
+            if (etHCNum.length() == 0) {
+                etHCNum.setError("Cannot be empty");
+            }
+            return false;
+        } else {
+            String password1 = etPassword.getText().toString().trim();
+            String password2 = etPassword_2.getText().toString().trim();
+            if (!(password1.equals(password2))) {
+                etPassword.setError("Passwords do not match");
+                etPassword_2.setError("Passwords do not match");
+                return false;
+            } else {
+                int selected = rbg.getCheckedRadioButtonId();
+                RadioButton genderBtn = (RadioButton) findViewById(selected);
+                String first_name = etFname.getText().toString().trim();
+                String last_name = etLname.getText().toString().trim();
+                String address = etAddress.getText().toString().trim();
+                String gender = genderBtn.getText().toString().trim();
+                dob.setTimeZone(TimeZone.getTimeZone("UTC"));
+                dob.set(mYear, mMonth, mDay);
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String HealthCard = etHCNum.getText().toString().trim();
 
-        int selected=rbg.getCheckedRadioButtonId();
-        RadioButton genderBtn=(RadioButton) findViewById(selected);
-        String first_name = etFname.getText().toString().trim();
-        String last_name = etLname.getText().toString().trim();
-        String address = etAddress.getText().toString().trim();
-        String gender = genderBtn.getText().toString().trim();
-        dob.setTimeZone(TimeZone.getTimeZone("UTC"));
-        dob.set(mYear, mMonth, mDay);
-        String email = etEmail.getText().toString().trim();
-        String password = etPassword.getText().toString().trim();
-        String HealthCard = etHCNum.getText().toString().trim();
+                API handler = new API(this);
+                handler.putUser(first_name, last_name, address, gender, dob, email, password, HealthCard);
 
-        API handler = new API(this);
-        handler.putUser(first_name,last_name,address,gender,dob,email,password,HealthCard);
+                return true;
+            }
+        }
     }
 
     public void onClick(View v) {
         if (v == butNext) {
-            registerUser();
-            Intent intent = new Intent(RegisterMainActivity.this, EmergencyContactActivity.class);
-            intent.putExtra("EMAIL", etEmail.getText().toString());
-            startActivity(intent);
-            finish();
+            if (registerUser() == true) {
+                Intent intent = new Intent(RegisterMainActivity.this, EmergencyContactActivity.class);
+                intent.putExtra("EMAIL", etEmail.getText().toString());
+                startActivity(intent);
+                finish();
+            }
         }else if(v == etDOB){
             Calendar c = Calendar.getInstance();
             c.setTimeZone(TimeZone.getTimeZone("UTC"));
